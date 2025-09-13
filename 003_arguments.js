d20a6d220c160ee3c91f6abba31b9f6b9833b0f1
Daniel Gao <1803483451@qq.com>
2025-09-13 21:35:12 +1000

    using arguments

---
+++ 003_arguments.js
@@ -0,0 +1,48 @@
+import { GraphQLFloat, GraphQLInt, GraphQLList, GraphQLNonNull, GraphQLObjectType, GraphQLSchema } from "graphql";
+import express from 'express';
+import { createHandler } from 'graphql-http/lib/use/express';
+import { ruruHTML } from 'ruru/server';
+
+
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'query',
+        fields: {
+            rollDice: {
+                type: new GraphQLList(GraphQLFloat),
+                args: {
+                    numDice: { type: new GraphQLNonNull(GraphQLInt) },
+                    numSides: { type: GraphQLInt }
+                },
+                resolve: (_, { numDice, numSides }) => {
+                    const output = [];
+                    for (let i = 0; i < numDice; i++) {
+                        output.push(Math.floor(Math.random() * numSides) + 1);
+                    }
+                    return output;
+                }
+            }
+        }
+    })
+});
+
+
+
+const app = express();
+
+app.all("/graphql", createHandler({ schema }));
+
+app.listen(4000);
+console.log("Server is running on port 4000");
+
+
+// Serve the GraphiQL IDE.
+app.get('/', (_req, res) => {
+    res.type('html');
+    res.end(ruruHTML({ endpoint: '/graphql' }));
+});
+
+/**
+ * call:
+ * { rollDice(numSides: 6, numDice:3) }
+ */
